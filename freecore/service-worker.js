48dd8d6a749f817d80da81d58fef7ac735cde497
askjasonautollc <askjasonautollc@gmail.com>
2025-06-07 15:58:02 -0400

    Update service-worker.js

--- freecore/service-worker.js
+++ freecore/service-worker.js
@@ -2,10 +2,12 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("askjason-cache").then((cache) => {
       return cache.addAll([
-        "./index.html",
-        "./icons/icon-192x192.png",
-        "./icons/icon-512x512.png",
-        "./manifest.json"
+        "/",
+        "/index.html",
+        "/freecore/index.html",
+        "/icons/icon-192x192.png",
+        "/icons/icon-512x512.png",
+        "/manifest.json"
       ]);
     })
   );

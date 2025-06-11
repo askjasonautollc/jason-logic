3d5406e3710426ae20d31faefc9cf7db8236d2d1
askjasonautollc <askjasonautollc@gmail.com>
2025-06-11 09:37:22 -0400

    Update submitEvaluation.js

--- api/submitEvaluation.js
+++ api/submitEvaluation.js
@@ -62,6 +62,9 @@ async function searchGoogle(query) {
 
 export default async function handler(req, res) {
   try {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     if (req.method === 'OPTIONS') return res.status(200).end();
     if (req.method !== 'POST') return res.status(405).json({ error: "Method not allowed" });
 
